a86a80e5b63838a7a7b60bff08bdf891cfbbdfec
awsdert <gb2985@gmail.com>
2021-03-11 22:43:39 +0000

    Basic design and setup done, next I will need to fix an issue with the curve offsets that only appeared after I copy-pasted the code and changed some variable names, after which I will begin putting in the draw code for the standard PI stuff before switching back to curvex, I'll work on speed tests later, the disabled input is just for quick access in javascript, they will not be enabled as they are supposed to just display values, not take input

---
+++ curvex.js
@@ -0,0 +1,276 @@
+function isFlt( val, def )
+{
+	val = Number.parseFloat(val);
+	if ( !(Number.isFinite(val)) )
+		return isFlt( def, 0 );
+	return val;
+}
+
+function isInt( val, def )
+{
+	val = Number.parseInt(val);
+	if ( !(Number.isInteger(val)) )
+		return isInt( def, 0 );
+	return val;
+}
+
+function createPositionsBuffer( gl, positions ) {
+
+	// Create a buffer for the square's positions.
+	const buff = gl.createBuffer();
+
+	// Select the positionBuffer as the one to apply buffer
+	// operations to from here out.
+	gl.bindBuffer( gl.ARRAY_BUFFER, buff );
+
+	// Now pass the list of positions into WebGL to build the
+	// shape. We do this by creating a Float32Array from the
+	// JavaScript array, then use it to fill the current buffer.
+	gl.bufferData(
+		gl.ARRAY_BUFFER,
+		new Float32Array( positions ),
+		gl.STATIC_DRAW
+	);
+
+	return buff;
+}
+
+function newPointXY( x, y )
+{
+	return { x : x, y : y };
+}
+
+function buildSolidTriangle( points, a, b, c )
+{
+	points.push( a.x, a.y, b.x, b.y, c.x, c.y );
+	return points;
+}
+
+function cxDraw( gl, app, vxaPoints, sides, width, height )
+{
+	var ret = { border: 0, html: "" };
+	var points = [];
+	gl.viewport( 0, 0, gl.canvas.width, gl.canvas.height );
+	gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+	gl.clear( gl.COLOR_BUFFER_BIT );
+	gl.useProgram( app );
+	gl.enableVertexAttribArray( vxaPoints );
+	var radius = width / 2
+		, square = radius * radius
+		, stop = sides / 4
+		, middle = stop / 2
+		, curve = 2
+		, div = 1 / stop
+		, frac = div / stop
+		, addmore = 1
+		, border_len = 0
+		, border_add1 = Math.sqrt(square * 2) / 2
+		, border_add2 = Math.sqrt(square * 3);
+	
+	stop -= 2;
+	console.log( "stop: " + stop + "; div: " + div + "; frac: " + frac );
+	for ( var i = 0, x = 0, y = width; i < stop; ++i )
+	{
+		var X = (x - 0.25), Xp = (X + div), Xm = (X - div);
+		var Y = (y - 1), Yp = (Y + div), Ym = (Y - div);
+		
+		console.log( "X = " + X + "; Y = " + Y );
+		points = buildSolidTriangle
+		(
+			points
+			, newPointXY( Xm, Ym )
+			, newPointXY( X, Y )
+			, newPointXY( Xp, Ym )
+		);
+		points = buildSolidTriangle
+		(
+			points
+			, newPointXY( -Xm, Ym )
+			, newPointXY( -X, Y )
+			, newPointXY( -Xp, Ym )
+		);
+		points = buildSolidTriangle
+		(
+			points
+			, newPointXY( Xm, -Ym )
+			, newPointXY( X, -Y )
+			, newPointXY( Xp, -Ym )
+		);
+		points = buildSolidTriangle
+		(
+			points
+			, newPointXY( -Xm, -Ym )
+			, newPointXY( -X, -Y )
+			, newPointXY( -Xp, -Ym )
+		);
+		x += frac + ((frac * curve) * (i == curve));
+		y -= frac + ((frac * (stop - curve)) * (i == curve));
+		curve += 2 * (i == curve);
+	}
+	
+	stop += 2;
+	var mine = ((border_add2 / (stop / 2.5)) + 5);
+	
+	border_len = stop / mine;
+	pi = border_len / 2;
+	
+	ret.border = border_len;
+	
+	const vxPoints = createPositionsBuffer( gl, points );
+	
+	gl.bindBuffer( gl.ARRAY_BUFFER, vxPoints );
+	
+	// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+	var size = 2;          // 2 components per iteration
+	var type = gl.FLOAT;   // the data is 32bit floats
+	var normalize = false; // don't normalize the data
+	var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+	var offset = 0;        // start at the beginning of the buffer
+	gl.vertexAttribPointer
+	(
+		vxaPoints
+		, size
+		, type
+		, normalize
+		, stride
+		, offset
+	);
+	
+	var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = points.length / 2;
+    gl.drawArrays( primitiveType, offset, count );
+	return ret;
+}
+
+function piDraw( gl, app, vxaPoints, sides, width, height )
+{
+	var ret = { border: 0, html: "" };
+	return ret;
+}
+
+function openWebGL( nBox )
+{
+	var gl = nBox.getContext("webgl");
+	gl = gl ? gl : nBox.getContext("2d");
+	if ( !gl )
+	{
+		alert("Couldn't open WebGL!");
+		return null;
+	}
+	return gl;
+}
+
+function newShader(gl, type, code) {
+	const shader = gl.createShader( type );
+
+	// Send the source to the shader object
+	gl.shaderSource( shader, code );
+
+	// Compile the shader program
+	gl.compileShader( shader );
+	
+	// See if it compiled successfully
+	if (!gl.getShaderParameter( shader, gl.COMPILE_STATUS) )
+	{
+		alert
+		(
+			'An error occurred compiling the shaders: '
+			+ gl.getShaderInfoLog( shader )
+		);
+		gl.deleteShader( shader );
+		return null;
+	}
+
+	return shader;
+}
+
+function newApp( gl, vsCode, fsCode )
+{
+	// Initialize a shader program, so WebGL knows how to draw our data
+	const vs = newShader( gl, gl.VERTEX_SHADER, vsCode );
+	const fs = newShader( gl, gl.FRAGMENT_SHADER, fsCode );
+	
+	const ctx = gl.createProgram();
+	gl.attachShader( ctx, vs );
+	gl.attachShader( ctx, fs );
+	gl.linkProgram( ctx );
+	
+	if ( !gl.getProgramParameter( ctx, gl.LINK_STATUS ) ) {
+		alert(
+			'Unable to initialize the ctx program: '
+			+ gl.getProgramInfoLog( ctx )
+		);
+		return null;
+	}
+	
+	return ctx;
+}
+
+var _cxGL = null, _piGL = null;
+
+const _vsCode = `
+	// an attribute will receive data from a buffer
+  attribute vec4 aPoints;
+
+  // all shaders have a main function
+  void main() {
+
+    // gl_Position is a special variable a vertex shader
+    // is responsible for setting
+    gl_Position = aPoints;
+  }`;
+	
+const _fsCode = `
+	// fragment shaders don't have a default precision so we need
+	// to pick one. mediump is a good default. It means "medium precision"
+	precision mediump float;
+
+	void main()
+	{
+		// gl_FragColor is a special variable a fragment shader
+		// is responsible for setting
+		gl_FragColor = vec4(1, 0, 0.5, 1); // return reddish-purple
+	}`;
+
+function main(form)
+{
+	var nSides = form.nSides, nWidth = form.nWidth, nHeight = form.nHeight;
+	var sides = isInt( nSides.value, 64 );
+	var width = isFlt( nWidth.value, 2 );
+	var height = isFlt( nHeight.value, 2 );
+	
+	for ( i = sides / 4; Math.floor(i) < i; ++sides, i = sides / 4 );
+	width += (width <= 0);
+	height += (height <= 0);
+	nSides.value = sides;
+	nWidth.value = width;
+	nHeight.value = height;
+	
+	var nhRadius = form.nhRadius, nvRadius = form.nvRadius;
+	var hradius = width / 2, vradius = height / 2;
+	nhRadius.value = hradius;
+	nvRadius.value = vradius;
+	
+	var cxBox = form.cxBox.lastElementChild.firstElementChild;
+	var piBox = form.piBox.lastElementChild.firstElementChild;
+	
+	var cxGL = openWebGL(cxBox, _cxGL), cxApp = newApp( cxGL, _vsCode, _fsCode );
+	var piGL = openWebGL(piBox, _piGL), piApp = newApp( piGL, _vsCode, _fsCode );
+	const cx_vxaPoints = cxGL.getAttribLocation ( cxApp, 'aPoints' );
+	const pi_vxaPoints = piGL.getAttribLocation ( piApp, 'aPoints' );
+	var cxOut = cxDraw( cxGL, cxApp, cx_vxaPoints, sides, width, height );
+	var piOut = piDraw( piGL, piApp, pi_vxaPoints, sides, width, height );
+	_cxGL = cxGL;
+	_piGL = piGL;
+	form.cxBorder.value = cxOut.border;
+	form.piBorder.value = piOut.border;
+	form.cxWidthPI.value = cxOut.border / width;
+	form.piWidthPI.value = piOut / width;
+	form.cxHeightPI.value = cxOut.border / height;
+	form.piHeightPI.value = piOut / height;
+	form.cxPI.value = cxOut.border / ((height / 2) + (width / 2));
+	form.piPI.value = piOut / ((height / 2) + (width / 2));
+	form.cxBox.lastElementChild.lastElementChild.innerHTML = cxOut.html;
+	form.piBox.lastElementChild.lastElementChild.innerHTML = piOut.html;
+}
